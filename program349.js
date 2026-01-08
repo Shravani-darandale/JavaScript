cd3691133851823bb596a39211513bdae6170116
Shravani-darandale <shravanidarandale52@gmail.com>
2026-01-08 12:06:33 +0530

    Add files via upload

---
+++ program349.js
@@ -0,0 +1,15 @@
+function CountDigit(no)
+{
+    
+}
+function main()
+{
+    let iValue = 721;
+
+    console.log(iValue);       
+    console.log(typeof(iValue));
+    console.log(iValue % 10);
+    console.log(iValue / 10);
+}
+
+main();
